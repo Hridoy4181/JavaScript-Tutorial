70fa6b0a028d10ac92a664b881b87d1aee916b28
Hridoy Rahman <ihridoyrahman@gmail.com>
2023-12-26 15:39:26 +0600

    js tutorial

---
+++ js/test.js
@@ -0,0 +1,10 @@
+// JS functions
+function sleep(name, time) {
+    console.log(name + " is sleeping from " + time);
+}
+
+sleep("Jasim", "10pm");
+sleep("Karim", "11pm");
+sleep("Rahim", "12pm");
+
+
